86576bd62c81149ab68e2540567765f8e92a67f9
AIGLEKIVOL <112876945+AIGLEKIVOL@users.noreply.github.com>
2022-11-21 13:13:40 +0100

    Create Position.java
    classe position, permet de simplifier la classe camIR

---
+++ src/polluxPak/Position.java
@@ -0,0 +1,36 @@
+package polluxPax
+
+public class Position {
+	private int x; 
+	private int y; 
+	
+	public Position(int abs, int ord) {
+		x=abs;
+		y=ord;
+	}
+	
+	public void setAbs(int abs) {
+		x=abs;
+	}
+	
+	public void setOrd(int ord) {
+		y=ord;
+	}
+	public int getAbs() {
+		return x;
+	}
+	
+	public int getOrd() {
+		return y;
+	}
+	
+	public int modulo() {
+		return Math.sqrt(x*x+y*y);
+	}
+	
+	public int arg() {
+		return Math.atan(y/x);
+	}
+	
+	
+}
